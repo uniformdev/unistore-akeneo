84db4e006ece011c6be9a4cbb1c28cbf34858414
Alex Shyba <alex.shyba@uniform.dev>
2022-10-02 10:43:36 -0700

    fix: header link

--- src/components/Navigation/components/HeaderNav/index.tsx
+++ src/components/Navigation/components/HeaderNav/index.tsx
@@ -5,17 +5,25 @@ import { sluggify } from '@/utils/stringUtils';
 
 const HeaderNav = () => (
   <div className="flex lg:flex-row flex-col lg:items-center justify-center lg:pl-5 pb-10 lg:pb-0">
-    {/* <TopNavLink href="/shop/shop-all" title="Shop all" /> */}
     <TopNavLink href="/fall2022" title="New collection" />
     {categoryUrls &&
-      categoryUrls.map((categoryLink: NavLinkProp) => <TopNavLink key={categoryLink.href} {...categoryLink} />)}
-    {/* <TopNavLink href="/contact" title="Contact" /> */}
+      categoryUrls.map((categoryLink: NavLinkProp) => (
+        <ProductCategoryLink key={categoryLink.href} {...categoryLink} />
+      ))}
   </div>
+);
+
+const ProductCategoryLink = ({ href, title }: NavLinkProp) => (
+  <NavLink
+    href={`/shop/${sluggify(href.replace('master_', ''))}`}
+    title={title}
+    className="header_footer_container--header_items text-black dark:text-white lg:m-0 m-3"
+  />
 );
 
 const TopNavLink = ({ href, title }: NavLinkProp) => (
   <NavLink
-    href={`/shop/${sluggify(href.replace('master_', ''))}`}
+    href={href}
     title={title}
     className="header_footer_container--header_items text-black dark:text-white lg:m-0 m-3"
   />
